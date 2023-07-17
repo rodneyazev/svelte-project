428f8fc505c492fa277261439e8995b5d1fb043a
rodneyazev <rodney.n.azevedo@gmail.com>
2023-07-17 01:52:21 -0300

    Final project

---
+++ svelte-project-3/src/lib/components/IngredienteSelecionavel.svelte
@@ -0,0 +1,33 @@
+<script lang="ts">
+    import { createEventDispatcher } from "svelte";
+    import Tag from "./Tag.svelte";
+    export let ingrediente: string;
+
+    const dispatch = createEventDispatcher<{
+        adicionarIngrediente: string,
+        removerIngrediente: string
+    }>();
+
+    let selecionado = false;
+
+    function aoClicar(){
+        selecionado = !selecionado;
+        if(selecionado){
+            // dispatch para adicionar ingrediente
+            dispatch('adicionarIngrediente', ingrediente);
+        } else {
+            dispatch('removerIngrediente', ingrediente);
+        }
+    }
+
+</script>
+
+<button on:click={aoClicar}>
+    <Tag ativa={selecionado}>{ingrediente}</Tag>
+</button>
+
+<style>
+    button {
+        cursor: pointer;
+    }
+</style>
