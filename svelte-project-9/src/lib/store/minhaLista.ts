e1d2144a07cd1017c698294c2a319901ababe5bf
rodneyazev <rodney.n.azevedo@gmail.com>
2023-07-17 21:48:35 -0300

    Working with svelte.transition

---
+++ svelte-project-9/src/lib/store/minhaLista.ts
@@ -0,0 +1,28 @@
+import { writable } from "svelte/store";
+
+const { subscribe, set, update } = writable<string[]>([]);
+
+
+export const minhaLista = {
+    subscribe,
+    set,
+    adicionarIngrediente(ingrediente: string){
+        update(((valorAtual) => [...valorAtual, ingrediente]));
+    },
+    removerIngrediente(ingrediente: string){
+        update((valorAtual) => valorAtual.filter((item) => item !== ingrediente));
+    }
+};
+
+
+
+
+
+
+/*
+
+minhaLista.set(['a','b','c'])
+
+minhaLista.update((valorAtualStore) => [...valorAtualStore, 'd'] )
+
+*/
